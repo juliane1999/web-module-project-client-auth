ca1bd6c9a0f8fbfca5833ef65a72683be2304540
juliane1999 <lastjuliane@gmail.com>
2021-08-17 17:42:43 -0700

    created project

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import PrivateRoute from './Components/PrivateRoute'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
